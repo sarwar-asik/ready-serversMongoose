b1c45b0ec119bbc2555755f16d1771c9297b5c4b
sarwar-asik <sarwarasik@gmail.com>
2024-10-15 15:28:09 +0600

    swager-optimized: reusable & optimized @api-design

---
+++ src/utils/schemaConverter.ts
@@ -0,0 +1,81 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Schema } from "mongoose";
+
+type SwaggerSchema= {
+    type: string;
+    description?: string;
+    items?: unknown;
+    enum?: string[];
+    default?: any;
+    properties?: Record<string, any>;
+    required?: string[];
+  }
+
+
+  function mapMongooseTypeToOpenAPI(type: any): string {
+    switch (type) {
+      case String:
+        return 'string';
+      case Number:
+        return 'number';
+      case Boolean:
+        return 'boolean';
+      case Date:
+        return 'string'; // Date is represented as string in OpenAPI
+      case Array:
+        return 'array';
+      case Object:
+        return 'object';
+      default:
+        return 'string';
+    }
+  }
+
+
+  function schemaConverter(mongooseSchema: Schema): SwaggerSchema {
+    const swaggerSchema: SwaggerSchema = {
+      type: 'object',
+      properties: {},
+      required: [],
+    };
+  
+    const schemaPaths = mongooseSchema.paths;
+  
+    Object.keys(schemaPaths).forEach((key) => {
+      const path = schemaPaths[key];
+      const fieldOptions = path.options;
+  
+      const  fieldSchema: SwaggerSchema = {
+        type: mapMongooseTypeToOpenAPI(fieldOptions.type),
+      };
+  
+      // Add additional field options like enum, default, etc.
+      if (fieldOptions.enum) {
+        fieldSchema.enum = fieldOptions.enum;
+      }
+  
+      if (fieldOptions.default !== undefined) {
+        fieldSchema.default = fieldOptions.default;
+      }
+  
+      if (fieldOptions.required) {
+        swaggerSchema.required?.push(key);
+      }
+  
+      // Handle arrays
+      if (fieldOptions.type instanceof Array && fieldOptions.type.length > 0) {
+        fieldSchema.type = 'array';
+        fieldSchema.items = {
+          type: mapMongooseTypeToOpenAPI(fieldOptions.type[0]),
+        };
+      }
+  
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      swaggerSchema.properties![key] = fieldSchema;
+    });
+  
+    return swaggerSchema;
+  }
+
+
+  export default schemaConverter;
--- src/utils/swagger.ts
+++ src/utils/swagger.ts
@@ -1,95 +1,55 @@
 import swaggerJsdoc from 'swagger-jsdoc';
 import path from 'path';
+import schemaConverter from './schemaConverter';
+import { UserSchema } from '../app/modules/USER/user.model';
+
 const options: swaggerJsdoc.Options = {
   definition: {
     openapi: '3.0.0',
     info: {
-      title: 'Welcome to Sarwar Server  API Documentation',
+      title: 'App Backend',
       version: '1.0.0',
-    },
-    components: {
-      securitySchemes: {
-        bearerAuth: {
-          type: 'http',
-          scheme: 'bearer',
-          bearerFormat: 'JWT',
-        },
-        apikeyAuth: {
-          type: 'apiKey',
-          name: 'Authorization',
-          in: 'header',
-          description:
-            'Custom token authorization: Use "Authorization: yourTokenHere"',
-        },
+      description:"API Documentation ",
+      contact:{
+        name:"Sarwar Hossain",
+        email:"sarwarasik@gmail.com",
+        url:"https://www.linkedin.com/in/sarwar-asik/"
       },
-      schemas: {
-        User: {
-          type: 'object',
-          properties: {
-            id: { type: 'string', description: 'User ID' },
-            email: { type: 'string', description: 'User email' },
-            userName: { type: 'string', description: 'User name' },
-            contactNumber: {
-              type: 'string',
-              description: 'User contactNumber',
-            },
-            img: { type: 'string', description: 'User img' },
-            details: { type: 'string', description: 'User details' },
-            location: { type: 'string', description: 'User location' },
-            points: { type: Number, description: 'User points' },
-            role: { type: 'enum', description: 'User Admin Super Admin' },
-
-            blood: { type: 'enum', description: 'AB+ AB- O+ O- A- A+ B+ B-' },
-            gender: { type: 'enum', description: 'Male Female' },
-          },
-        },
-        Plan: {
-          type: 'object',
-          properties: {
-            title: {
-              type: 'string',
-              description: 'The title of the plan',
-            },
-            goalType: {
-              type: 'string',
-              description: 'The type of goal for the plan',
-            },
-            points: {
-              type: 'number',
-              description: 'Points associated with the plan',
-            },
-          },
-        },
-        PlanInput: {
-          type: 'object',
-          properties: {
-            title: {
-              type: 'string',
-              description: 'The title of the plan',
-            },
-            goalType: {
-              type: 'string',
-              description: 'The type of goal for the plan',
-            },
-            points: {
-              type: 'number',
-              description: 'Points associated with the plan',
-            },
-          },
-        },
+      license: {
+        name: 'Spark Tech',
+        url: 'https://sparktech.agency/',
       },
     },
-    security: [
-      {
-        bearerAuth: [],
+       // !component part
+       components: {
+        securitySchemes: {
+          bearerAuth: {
+            type: 'http',
+            scheme: 'bearer',
+            bearerFormat: 'JWT',
+          },
+        },
+        schemas: {
+          UserSchema:schemaConverter(UserSchema), 
+        },
+        response:{
+          description: 'Access token is missing or invalid',
+          content: {
+            'application/json': {
+              example: { message: 'Unauthorized' },
+            },
+          },
+        }
       },
-    ],
+       // !security part
+      security: [
+        {
+          bearerAuth: [],
+        },
+      ],
   },
-  // apis: ['../app/**/*.ts', '../app.ts'], // Adjust the path to your route files
-  // apis: ['./app/modules/**/*.ts'],
-  // apis:["../app/routes/index.ts","../app/modules/**/*.ts","../app.ts",'./swagger.routes.ts'],
   apis: [
-    path.join(__dirname, '../app/routes/*.ts'), // Adjust path as necessary
+    path.join(__dirname, '../app/routes/*.ts'), 
     path.join(__dirname, '../app/modules/**/*.ts'),
     path.join(__dirname, '../app.ts'),
   ],
